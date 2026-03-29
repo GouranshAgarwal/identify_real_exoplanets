aa111586619e0c83ddbfa3a5ddb06cb8ef5af8c9
GouranshAgarwal <145385192+GouranshAgarwal@users.noreply.github.com>
2026-03-30 01:33:22 +0530

    render Url set

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://identify-real-exoplanets.onrender.com"
 });
 
 export const predictPlanet = async(data) => {
